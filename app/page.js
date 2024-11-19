85428c7fad33d4b132e8ded21658214f28502569
Hadia Khalil <168618032+HadiaKhalil@users.noreply.github.com>
2024-11-19 15:54:54 +0500

    first-commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+
+import Navbar from './Navbar/page'
+import Hero from './hero/page'
+import Second from './second/page'
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Second/>
+    </div>
+  )
+}
+
+export default page
